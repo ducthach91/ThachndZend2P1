b31c6834920acc812755882dc28ddb234b296c2e
ducthach91 <ducthach91@gmail.com>
2015-09-12 11:16:14 +0700

    application

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -89,7 +89,7 @@ return array(
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'template_path_stack' => array(
-            __DIR__ . '/../view',
+            'application' => __DIR__ . '/../view',
         ),
     ),
     // Placeholder for console routes
